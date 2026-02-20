0b8cea07f1b9010ba78e1cbfd2389071000cbde6
rodri <rodri@Groot_Rod>
2026-02-20 12:43:32 +0100

    PRimer commit

--- src/daa1/Complejidad.java
+++ src/daa1/Complejidad.java
@@ -3,6 +3,10 @@ package daa1;
 import java.Utilities.ArrayList;
 
 public class Complejidad {
-
-
+    public void ordenaBurbuja(){
+        int i,j,auux;
+        for ( i=0; i<VectorEneto.1.length -1; 1++){
+            for ()
+    }
+    }
 }
